cc3ce76f2418bbbf5a95a6845b8bff5a09fd8f12
onlyH <736087284@qq.com>
2019-01-30 15:37:39 +0800

    完善第一版面内容

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -3,21 +3,29 @@
     <Heador/>
     <Slider/>
     <Novice/>
+    <Borrow/>
+    <Money/>
+    <Product/>
+    <Left/>
     <Footor/>
     <Navbar/>
   </div>
 </template>
 
 <script>
-  import Heador from "../public/header.vue";
-  import Navbar from "../public/navbar.vue";
-  import Footor from '../public/footer.vue';
-  import Slider from "./hslider.vue";
+  import Heador from "../public/header.vue"
+  import Navbar from "../public/navbar.vue"
   import Novice from "./novice.vue"
+  import Borrow from "./borrow.vue"
+  import Money from "./money.vue"
+  import Product from './product.vue'
+  import Left from "./life.vue"
+  import Footor from '../public/footer.vue'
+  import Slider from "./hslider.vue"
 
   export default {
     components: {
-      Heador, Footor, Navbar, Slider, Novice
+      Heador, Footor, Navbar, Slider, Novice, Borrow, Money, Product, Left
     }
   };
 </script>
